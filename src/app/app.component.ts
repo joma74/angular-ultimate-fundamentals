d120714efc407a98083b9b20960e8088cc989f14
joma74 <dev-mails@gmx.net>
2018-06-23 17:19:19 +0200

    chore(refactor): files linted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core"
+import { Component } from '@angular/core'
 
-import "../assets/css/styles.css"
+import '../assets/css/styles.css'
 
 @Component({
-	selector: "my-app",
-	templateUrl: "./app.component.html",
-	styleUrls: ["./app.component.css"],
+	selector: 'my-app',
+	styleUrls: ['./app.component.css'],
+	templateUrl: './app.component.html',
 })
 export class AppComponent {}
