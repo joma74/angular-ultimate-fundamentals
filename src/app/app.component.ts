5d631c2475a440387953eadb288d0e14eb2e4abb
joma74 <dev-mails@gmx.net>
2018-07-14 22:14:53 +0200

    feat: see how scss works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core'
+import { Component } from "@angular/core"
 
-import '../assets/css/styles.css'
+import "../assets/css/styles.css"
 
 @Component({
-	selector: 'my-app',
-	styleUrls: ['./app.component.css'],
-	templateUrl: './app.component.html',
+	selector: "my-app",
+	styleUrls: ["./app.component.scss"],
+	templateUrl: "./app.component.html",
 })
-export class AppComponent {}
+export class AppComponent {
+	public title: string
+	public major: number = 4
+	public minor: number = 4
+	public patch: number = 7
+	public isHappy: boolean
+	public logo: string = "assets/images/angular.png"
+	public name: string = "Jo"
+	constructor() {
+		this.title = "Hello"
+		this.isHappy = false
+	}
+	public handleBlur(event: any) {
+		this.name = event.target.value
+	}
+	public handleInput(event: any) {
+		this.name = event.target.value
+	}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { CommonModule } from "@angular/common"
+import { NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
 
-import { AppComponent } from './app.component'
+import { AppComponent } from "./app.component"
 
 @NgModule({
 	bootstrap: [AppComponent],
 	declarations: [AppComponent],
-	imports: [BrowserModule],
+	imports: [BrowserModule, CommonModule],
 })
 export class AppModule {}
