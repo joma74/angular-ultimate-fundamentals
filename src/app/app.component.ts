4bb2124583142b12945517c7ee0fdc47e4ceb653
joma74 <dev-mails@gmx.net>
2018-07-16 00:52:08 +0200

    feat: two-way databinding banana

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,12 @@ import "../assets/css/styles.css"
 	templateUrl: "./app.component.html",
 })
 export class AppComponent {
-	public title: string
-	public major: number = 4
-	public minor: number = 4
-	public patch: number = 7
-	public isHappy: boolean
-	public logo: string = "assets/images/angular.png"
 	public name: string = "Jo"
-	constructor() {
-		this.title = "Hello"
-		this.isHappy = false
+
+	public handleClick(event: any) {
+		this.name = "Jo"
 	}
-	public handleBlur(event: any) {
-		this.name = event.target.value
-	}
-	public handleInput(event: any) {
-		this.name = event.target.value
+	public handleChange(value: string) {
+		this.name = value
 	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
+import { FormsModule } from "@angular/forms"
 import { BrowserModule } from "@angular/platform-browser"
 
 import { AppComponent } from "./app.component"
@@ -7,6 +8,6 @@ import { AppComponent } from "./app.component"
 @NgModule({
 	bootstrap: [AppComponent],
 	declarations: [AppComponent],
-	imports: [BrowserModule, CommonModule],
+	imports: [BrowserModule, CommonModule, FormsModule],
 })
 export class AppModule {}
