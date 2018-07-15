c4b61625cef4150e73f82c717884df916afb9c1b
joma74 <dev-mails@gmx.net>
2018-07-16 01:00:24 +0200

    fix: can*t bind to ngModel in test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from "@angular/core/testing"
-
+import { FormsModule } from "@angular/forms"
 import { AppComponent } from "./app.component"
 
 describe("App", () => {
 	beforeEach(() => {
-		TestBed.configureTestingModule({ declarations: [AppComponent] })
+		TestBed.configureTestingModule({
+			declarations: [AppComponent],
+			imports: [FormsModule],
+		})
 	})
 
 	it("should work", () => {
