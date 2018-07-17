21ece9e998c82b8af6d62dcd04ea48a33789500a
joma74 <dev-mails@gmx.net>
2018-07-18 01:32:06 +0200

    feat: null-safe property paths in ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,12 @@ interface IPassenger {
 	fullName: string
 	checkedIn: boolean
 	checkInDate?: number
+	children?: IChild[]
+}
+
+interface IChild {
+	name: string
+	age: number
 }
 
 @Component({
@@ -20,12 +26,14 @@ export class AppComponent {
 		{
 			checkInDate: null,
 			checkedIn: false,
+			children: [{ name: "Ted", age: 13 }, { name: "Alice", age: 18 }],
 			fullName: "Rose",
 			id: 1,
 		},
 		{
 			checkInDate: 149160600000,
 			checkedIn: true,
+			children: null,
 			fullName: "Stephen",
 			id: 2,
 		},
