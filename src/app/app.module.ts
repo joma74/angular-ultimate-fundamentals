4afbe0324405f75f8df79c180c668e76e2e3b3b0
joma74 <dev-mails@gmx.net>
2018-07-18 17:34:06 +0200

    (feat) add a custom module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 
+import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module"
+
 import { AppComponent } from "./app.component"
 
 @NgModule({
 	bootstrap: [AppComponent],
 	declarations: [AppComponent],
-	imports: [BrowserModule, CommonModule],
+	imports: [BrowserModule, CommonModule, PassengerDashboardModule],
 })
 export class AppModule {}
