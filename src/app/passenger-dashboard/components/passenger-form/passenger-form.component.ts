7428552aa75d17119b1c27512bb2377d2433761c
joma74 <dev-mails@gmx.net>
2018-07-27 14:45:16 +0200

    (fix) select-dropdown-chevron is not in the right spot

--- src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -91,7 +91,7 @@ import { IPassenger } from "../../models/passenger.interface"
 							{{ item.value }}
 						</option>
 					</select>
-					<div class="pointer-events-none absolute pin-y pin-r flex items-center text-grey-darker">
+					<div class="select-dropdown-chevron">
 						<svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
 					</div>
 				</div>
